fe9878cd46956e49f864ec644f496e54c53b6ea6
kjasmin9 <151543369+kjasmin9@users.noreply.github.com>
2024-07-21 10:47:50 +0100

    Create create_single_csv.py

---
+++ part_1/create_single_csv.py
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+import csv
+import os
+
+# define the path to the input CSV file
+input_csv_path = 'Path/to/updated_mirnas_of.interest.csv'
+
+# define and create the directory to store the output files
+output_dir = 'Desired/path/to/mirnas_of_interest_482''
+os.makedirs(output_dir, exist_ok=True)
+
+# read the input CSV file
+with open(input_csv_path, newline='') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        content = row[0]
+        filename = f"{content}.csv"
+        file_path = os.path.join(output_dir, filename)
+
+        # write the content to the new file
+        with open(file_path, 'w', newline='') as newfile:
+            writer = csv.writer(newfile)
+            writer.writerow([content])
+
+print(f'Written .csv file can be found in {output_dir}')
